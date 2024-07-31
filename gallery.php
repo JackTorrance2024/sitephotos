28b435d6e2f2f80a44a056668a650f2152f79446
JackTorrance2024 <theshining@free.fr>
2024-07-31 17:56:02 +0200

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -1,10 +1,7 @@
 <?php
-header('Content-Type: application/json');
-
-// Error reporting for debugging
+// Erreur de rapport pour débogage
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-
 error_reporting(E_ALL);
 
 $directory = 'photos';
@@ -18,18 +15,40 @@ if (is_dir($directory)) {
         }
     }
 } else {
-    // Error message if the directory is not found
+    // Message d'erreur si le répertoire n'est pas trouvé
     echo json_encode(array('error' => 'Directory not found'));
     exit;
 }
-
-// Check for errors or unexpected output
-$output = json_encode($photos);
-if ($output === false) {
-    // JSON encoding error
-    echo json_encode(array('error' => 'JSON encoding error: ' . json_last_error_msg()));
-    exit;
-}
-
-echo $output;
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Galerie de Photos</title>
+    <link rel="stylesheet" href="gallery.css">
+    <link rel="shortcut icon" type="image/x-icon" href="logo.jpg" />
+</head>
+<body>
+<div class="gallery">
+    <?php foreach ($photos as $photo): ?>
+        <?php
+        $filepath = $directory . '/' . htmlspecialchars($photo);
+        $imagesize = getimagesize($filepath);
+        $largeur = $imagesize[0];
+        $hauteur = $imagesize[1];
+
+        if ($hauteur > $largeur) {
+            echo '<div class="blocv">';
+            echo '<img src="' . $filepath . '" alt="Photo" class="verticale">';
+            echo '</div>';
+        } else {
+            echo '<div class="bloch">';
+            echo '<img src="' . $filepath . '" alt="Photo" class="horizontale">';
+            echo '</div>';
+        }
+        ?>
+    <?php endforeach; ?>
+</div>
+</body>
+</html>
